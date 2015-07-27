fb95cc1427bc4bbe6cfd13cd6bdf93d8661f0d5f
Adam Chelminski <adam.chelminski@mongodb.com>
2015-07-27 15:12:56 -0400

    Sketchy but will probably work on windows

--- src/mongo/db/fts/unicode/string.cpp
+++ src/mongo/db/fts/unicode/string.cpp
@@ -32,20 +32,14 @@
 #include <codecvt>
 #include <locale>
 
-#if defined(_MSC_VER) && _MSC_VER < 1900
 #include "mongo/shell/linenoise_utf8.h"
-#endif
-
 #include "mongo/util/assert_util.h"
 
 namespace mongo {
 namespace unicode {
 
-#if defined(_MSC_VER) && _MSC_VER < 1900
 using linenoise_utf8::copyString32to8;
 using linenoise_utf8::copyString8to32;
-typedef int char32_t;
-#endif
 
 using std::codecvt;
 using std::codecvt_base;
@@ -56,49 +50,21 @@ using std::u32string;
 using std::use_facet;
 
 String::String(const StringData utf8_src) {
-    // _data is the target, resize it so that it's guaranteed to fit all of the input characters.
-    _data.resize(utf8_src.size());
+    // _data is the target, resize it so that it's guaranteed to fit all of the input characters,
+    // plus a null character if there isn't one.
+    _data.resize(utf8_src.size() + 1);
 
-#if defined(_MSC_VER) && _MSC_VER < 1900
     int result = 0;
     size_t resultSize = 0;
 
-    copyString8to32(
-        static_cast<int*>(&_data[0]), &utf8_src.rawData()[0], _data.size(), resultSize, result);
+    copyString8to32(reinterpret_cast<unsigned int*>(&_data[0]),
+                    reinterpret_cast<const unsigned char*>(&utf8_src.rawData()[0]),
+                    _data.size(),
+                    resultSize,
+                    result);
 
     uassert(28732, "text contains invalid UTF-8", result == 0);
-#else
-    auto& conversion_facet = use_facet<codecvt<char32_t, char, mbstate_t>>(locale::classic());
-    mbstate_t mb = mbstate_t();
 
-    // Apply the UTF-8 -> UTF-32 conversion.
-    const char* from_next;
-    char32_t* to_next;
-    codecvt_base::result result = conversion_facet.in(mb,
-                                                      &utf8_src.rawData()[0],
-                                                      &utf8_src.rawData()[utf8_src.size()],
-                                                      from_next,
-                                                      &_data[0],
-                                                      &_data[_data.size()],
-                                                      to_next);
-
-    // Handle invalid UTF-8.
-    switch (result) {
-        case codecvt_base::ok:
-            // Success
-            break;
-        case codecvt_base::partial:
-        case codecvt_base::error:
-            // Failure
-            uasserted(28733, "text contains invalid UTF-8");
-            break;
-        case codecvt_base::noconv:
-            invariant(false);
-            break;
-    }
-
-    size_t resultSize = to_next - &_data[0];
-#endif
     // Resize _data so it is only as big as what it contains.
     _data.resize(resultSize);
 }
@@ -106,43 +72,12 @@ String::String(const StringData utf8_src) {
 String::String(u32string&& src) : _data(std::move(src)) {}
 
 std::string String::toString() const {
-#if defined(_MSC_VER) && _MSC_VER < 1900
-    std::string output(_data.size() * 4, '\0');
-    size_t resultSize = copyString32to8(&output[0], &_data[0], output.size());
-#else
-    auto& conversion_facet = use_facet<codecvt<char32_t, char, mbstate_t>>(locale::classic());
-    mbstate_t mb = mbstate_t();
-
-    // output is the target, size it so that it's guaranteed to fit all of the input characters.
-    std::string output(_data.size() * conversion_facet.max_length(), '\0');
-
-    // Apply the UTF-32 -> UTF-8 conversion.
-    const char32_t* from_next;
-    char* to_next;
-    codecvt_base::result result = conversion_facet.out(mb,
-                                                       &_data[0],
-                                                       &_data[_data.size()],
-                                                       from_next,
-                                                       &output[0],
-                                                       &output[output.size()],
-                                                       to_next);
-
-    // Ensure that there is no invalid UTF-32.
-    switch (result) {
-        case codecvt_base::ok:
-            break;
-        case codecvt_base::partial:
-        case codecvt_base::error:
-        case codecvt_base::noconv:
-            // If this invariant is triggered, it means that for some reason, one of the char32_t
-            // components of the u32string is greater than 0x10ffff, which is outside the range of
-            // Unicode code points. Check your mappings and ensure they are all valid codepoints.
-            invariant(false);
-            break;
-    }
-    size_t resultSize = to_next - &output[0];
-
-#endif
+    // output is the target, resize it so that it's guaranteed to fit all of the input characters,
+    // plus a null character if there isn't one.
+    std::string output(_data.size() * 4 + 1, '\0');
+    size_t resultSize = copyString32to8(reinterpret_cast<unsigned char*>(&output[0]),
+                                        reinterpret_cast<const unsigned int*>(&_data[0]),
+                                        output.size());
 
     // Resize output so it is only as large as what it contains.
     output.resize(resultSize);
