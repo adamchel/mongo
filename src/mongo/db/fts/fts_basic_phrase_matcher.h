5c8b9cde18fa09c84a21bc3c4c7c340e38623f6a
Adam Chelminski <adam.chelminski@mongodb.com>
2015-07-29 11:22:45 -0400

    add things that got lost in the merge

--- src/mongo/db/fts/fts_basic_phrase_matcher.h
+++ src/mongo/db/fts/fts_basic_phrase_matcher.h
@@ -36,7 +36,8 @@ namespace fts {
 
 /**
  * A phrase matcher that looks for exact substring matches with optional ASCII-aware case
- * insensitivity.
+ * insensitivity. This phrase matcher does not implement the kDiacriticSensitive match option. All
+ * operations are inherently diacritic sensitive.
  */
 class BasicFTSPhraseMatcher final : public FTSPhraseMatcher {
     MONGO_DISALLOW_COPYING(BasicFTSPhraseMatcher);
